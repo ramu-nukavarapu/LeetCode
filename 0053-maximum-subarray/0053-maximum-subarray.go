918ad83ffcc8dfbbb2a9715fbd87b8e326ef260b
ramu-nukavarapu <141512847+ramu-nukavarapu@users.noreply.github.com>
2024-03-05 18:07:04 +0530

    Time: 90 ms (48.96%), Space: 7.9 MB (87.73%) - LeetHub

---
+++ 0053-maximum-subarray/0053-maximum-subarray.go
@@ -0,0 +1,14 @@
+func maxSubArray(nums []int) int {
+    max := math.MinInt64
+    sum := 0
+    for i:=0; i<len(nums); i++ {
+        sum += nums[i]
+        if(sum > max){
+            max = sum
+        }
+        if(sum < 0){
+            sum = 0
+        }
+    }
+    return max
+}
