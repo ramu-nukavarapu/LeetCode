6c17d0cc4d556dc3dc3e2c317cc47a813c30018f
ramu-nukavarapu <141512847+ramu-nukavarapu@users.noreply.github.com>
2024-03-24 10:48:02 +0530

    Time: 3 ms (63.90%), Space: 2.7 MB (12.01%) - LeetHub

---
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.go
@@ -0,0 +1,25 @@
+func search(nums []int, target int) int {
+    low := 0
+    high := len(nums)-1
+    var mid int
+    for low<=high{
+        mid = (low+high)/2;
+        if(nums[mid] == target){
+            return mid;
+        }
+        if(nums[low] <= nums[mid]){
+            if(nums[low] <= target && target <= nums[mid]){
+                high = mid-1
+            }else{
+                low = mid+1
+            }
+        }else{
+            if(nums[mid] <= target && target <= nums[high]){
+                low = mid+1
+            }else{
+                high = mid-1
+            }
+        }
+    }
+    return -1
+}
