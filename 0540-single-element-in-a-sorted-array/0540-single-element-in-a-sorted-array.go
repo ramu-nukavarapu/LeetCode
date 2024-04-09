513c0cd58c5c7f482ea9c9ff70cf90fa319d4554
ramu-nukavarapu <141512847+ramu-nukavarapu@users.noreply.github.com>
2024-04-09 17:39:57 +0530

    Time: 16 ms (82.29%), Space: 7.6 MB (21.14%) - LeetHub

--- 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.go
+++ 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.go
@@ -14,24 +14,14 @@ func singleNonDuplicate(nums []int) int {
     var mid int
     for(low<=high){
         mid=(low+high)/2
-        if(mid%2 == 0){
-            if(nums[mid+1]==nums[mid]){
-                low = mid+1
-            }else if(nums[mid-1]==nums[mid]){
-                high = mid-1
-            }else{
-                return nums[mid]
-            }
+        if((nums[mid] != nums[mid-1]) && (nums[mid] != nums[mid+1])){
+            return nums[mid]
+        }
+        if((mid%2 == 0 && nums[mid+1]==nums[mid]) || (mid%2==1 && nums[mid-1]==nums[mid])){
+            low = mid+1
         }else{
-            if(nums[mid+1]==nums[mid]){
-                high = mid-1
-            }else if(nums[mid-1]==nums[mid]){
-                low = mid+1
-            }else{
-                return nums[mid]
-            }
-            
+            high = mid-1
         }
     }
-    return nums[mid]
+    return -1
 }
