967cd167c76794773596941bec0f4f744e2e5643
ramu-nukavarapu <141512847+ramu-nukavarapu@users.noreply.github.com>
2024-05-11 23:01:37 +0530

    Time: 0 ms (100.00%), Space: 44.1 MB (82.07%) - LeetHub

--- 0142-linked-list-cycle-ii/0142-linked-list-cycle-ii.java
+++ 0142-linked-list-cycle-ii/0142-linked-list-cycle-ii.java
@@ -11,30 +11,50 @@
  */
 public class Solution {
     public ListNode detectCycle(ListNode head) {
-        HashMap<ListNode,Integer> hp = new HashMap<>();
-        ListNode temp = head;
-        int i = 0,pos = -1;
-        while(temp != null){
-            if(hp.containsKey(temp)){
-                pos = hp.get(temp);
-                break;
-            }else{
-                hp.put(temp,i);
-                i++;
-            }
-            temp = temp.next;
-        }
-        if(pos != -1){
-            temp = head;
-            i = 0;
-            while(i <= pos && temp != null){
-                if(i == pos){
-                    return temp;
+        ListNode result = null;
+        if(head == null || head.next == null)
+            return result;
+        ListNode slow = head;
+        ListNode fast = head;
+        while(fast != null && fast.next != null){
+            slow = slow.next;
+            fast = fast.next.next;
+            
+            if(slow == fast){
+                slow = head;
+                while(slow != fast){
+                    slow = slow.next;
+                    fast = fast.next;
                 }
-                temp = temp.next;
-                pos--;
+                result = fast;
+                return result;
             }
         }
-        return null;
+        return result;
     }
 }
+// HashMap<ListNode,Integer> hp = new HashMap<>();
+//         ListNode temp = head;
+//         int i = 0,pos = -1;
+//         while(temp != null){
+//             if(hp.containsKey(temp)){
+//                 pos = hp.get(temp);
+//                 break;
+//             }else{
+//                 hp.put(temp,i);
+//                 i++;
+//             }
+//             temp = temp.next;
+//         }
+//         if(pos != -1){
+//             temp = head;
+//             i = 0;
+//             while(i <= pos && temp != null){
+//                 if(i == pos){
+//                     return temp;
+//                 }
+//                 temp = temp.next;
+//                 pos--;
+//             }
+//         }
+//         return null;
